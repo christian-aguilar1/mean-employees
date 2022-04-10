0f35494a1a95b89926fbd8786bc87f8b537a4008
christian-aguilar1 <christianaguilarm1@gmail.com>
2022-04-10 17:03:59 -0400

    defining the project structure and ready server

---
+++ server/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.set("port", process.env.PORT || 4000);
+
+app.use(morgan("dev"));
+
+module.exports = app;
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,5 @@
-console.log("init project");
+const app = require("./app");
+
+app.listen(app.get("port"));
+
+console.log("server listening on port", app.get("port"));
