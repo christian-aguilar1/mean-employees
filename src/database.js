3b54668c0736eb0606b0d288949d5e7867565b82
christian-aguilar1 <christianaguilarm1@gmail.com>
2022-04-10 21:28:52 -0400

    finished the employee api and created the database models

--- src/database.js
+++ src/database.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect("mongodb://localhost/mean-employees", {
-    useNewUrlParser: true,
     useUnifiedTopology: true,
+    useNewUrlParser: true,
   })
   .then((db) => console.log("Database connected"))
   .catch((err) => console.log(err));
